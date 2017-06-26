2e96726e1b3d1b54e0f0c8c69356145716966012
sdmay10 <sdmay10@gmail.com>
2017-06-26 14:03:51 -0400

    Working Search and Display

---
+++ src/app/app-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+@Injectable()
+export class AppServiceService {
+
+  constructor(private http: Http) { }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
+
 import { Http, Response } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { Observable } from 'rxjs/Observable';
 
@@ -14,6 +16,24 @@ import 'rxjs/add/operator/map';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+title = 'Whatchu Want?';
+link1 = 'https://api.giphy.com/v1/gifs/search?q=';
+link2 = '&api_key=dc6zaTOxFJmzC&limit=10';
+http: Http;
+gifs = [];
 
+constructor(http: Http) {
+  this.http = http;
+}
+
+
+  searchClick(searchTerm: HTMLInputElement): void {
+const searchLink = this.link1 + searchTerm.value + this.link2;
+
+this.http.get(searchLink)
+  .subscribe((res: Response) => {
+  this.gifs = res.json().data;
+  });
+  }
 
 }
